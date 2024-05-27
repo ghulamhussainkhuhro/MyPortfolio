d11b2afe436b0b237c0a20e3066ad795dae6b49b
Ghulam Hussain Khuhro <157501553+ghulamhussainkhuhro@users.noreply.github.com>
2024-05-28 01:29:17 +0500

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/second.jpg";
+import image from "../images/motion-background.jpg";
 
 const imageAltText = "Ghulam Hussain Khuhro Data and BI Analyst. This is a backgound image of my about section";
 
